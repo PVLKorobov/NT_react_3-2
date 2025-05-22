6bdb72ee9bbf27f4074b0be0a3798177eb08194a
Pavel Korobov <korobovpvl@gmail.com>
2025-05-22 22:57:09 +0300

    task

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,118 @@
 import './App.css'
 import listingData from './assets/data/etsy.json'
+import Listing from './Listing'
 
-interface ListingImage {
-  listing_image_id:number,
-  hex_code:string,
-  red:number,
-  green:number,
-  blue:number,
-  hue:number,
-  saturation:number,
-  brightness:number,
-  is_black_and_white:boolean,
-  creation_tsz:number,
-  listing_id:number,
-  rank:string,
-  url_75x75:string,
-  url_170x135:string,
-  url_570xN:string,
-  url_fullxfull:string,
-  full_height:number,
-  full_width:number
-}
-
-interface ListingItem {
-    listing_id:number,
-    state:string,
-    user_id:number,
-    category_id:number,
-    title:string,
-    description:string,
-    creation_tsz:number,
-    ending_tsz:number,
-    original_creation_tsz:number,
-    last_modified_tsz:number,
-    price:number,
-    currency_code:string,
-    quantity:number,
-    sku:string[],
-    tags:string[],
-    category_path:string[],
-    category_path_ids:number[],
-    materials:string[],
-    shop_section_id:number,
-    featured_rank:string,
-    state_tsz:number,
-    url:string,
-    views:number,
-    num_favorers:number,
-    shipping_template_id:number,
-    processing_min:number,
-    processing_max:number,
-    who_made:string,
-    is_supply:boolean,
-    when_made:string,
-    item_weight:number,
-    item_weight_unit:string,
-    item_length:number,
-    item_width:number,
-    item_height:number,
-    item_dimensions_unit:string,
-    is_private:boolean,
-    recipient:string,
-    occasion:string,
-    style:string,
-    non_taxable:boolean,
-    is_customizable:boolean,
-    is_digital:boolean,
-    file_data:string,
-    should_auto_renew:boolean,
-    language:string,
-    has_variations:boolean,
-    taxonomy_id:number,
-    taxonomy_path:string[],
-    used_manufacturer:boolean,
-    MainImage:ListingImage
-}
-
-
-// function App() {
-//   const items = JSON.parse(listingData)
-
-//   return (
-
-//   )
+// interface ListingImage {
+//   listing_image_id:number,
+//   hex_code:string,
+//   red:number,
+//   green:number,
+//   blue:number,
+//   hue:number,
+//   saturation:number,
+//   brightness:number,
+//   is_black_and_white:boolean,
+//   creation_tsz:number,
+//   listing_id:number,
+//   rank:string,
+//   url_75x75:string,
+//   url_170x135:string,
+//   url_570xN:string,
+//   url_fullxfull:string,
+//   full_height:number,
+//   full_width:number
 // }
 
-// export {App}
-export type {ListingItem, ListingImage}
+// interface ListingItem {
+//     listing_id:number,
+//     state:string,
+//     user_id:number,
+//     category_id:number,
+//     title:string,
+//     description:string,
+//     creation_tsz:number,
+//     ending_tsz:number,
+//     original_creation_tsz:number,
+//     last_modified_tsz:number,
+//     price:number,
+//     currency_code:string,
+//     quantity:number,
+//     sku:string[],
+//     tags:string[],
+//     category_path:string[],
+//     category_path_ids:number[],
+//     materials:string[],
+//     shop_section_id:number,
+//     featured_rank:string,
+//     state_tsz:number,
+//     url:string,
+//     views:number,
+//     num_favorers:number,
+//     shipping_template_id:number,
+//     processing_min:number,
+//     processing_max:number,
+//     who_made:string,
+//     is_supply:boolean,
+//     when_made:string,
+//     item_weight:number,
+//     item_weight_unit:string,
+//     item_length:number,
+//     item_width:number,
+//     item_height:number,
+//     item_dimensions_unit:string,
+//     is_private:boolean,
+//     recipient:string,
+//     occasion:string,
+//     style:string,
+//     non_taxable:boolean,
+//     is_customizable:boolean,
+//     is_digital:boolean,
+//     file_data:string,
+//     should_auto_renew:boolean,
+//     language:string,
+//     has_variations:boolean,
+//     taxonomy_id:number,
+//     taxonomy_path:string[],
+//     used_manufacturer:boolean,
+//     MainImage:ListingImage
+// }
+
+interface ListingItemData {
+  listing_id:number,
+  url:string,
+  MainImage_url_570xN:string,
+  title:string,
+  currency_code:string,
+  price:number,
+  quantity:number
+}
+
+
+function App() {
+  const listingItems = listingData
+  let FilteredLisingItems:ListingItemData[]  = listingItems.filter(
+    listingItemData => listingItemData.listing_id !== undefined &&
+    listingItemData.url !== undefined &&
+    listingItemData.MainImage !== undefined &&
+    listingItemData.title !== undefined &&
+    listingItemData.currency_code !== undefined &&
+    listingItemData.price !== undefined &&
+    listingItemData.quantity !== undefined)
+  .map(listingItemData => ({
+    listing_id: listingItemData.listing_id,
+    url: listingItemData.url,
+    MainImage_url_570xN: listingItemData.MainImage.url_570xN,
+    title: listingItemData.title,
+    currency_code: listingItemData.currency_code,
+    price: listingItemData.price,
+    quantity: listingItemData.quantity
+  }))
+
+  return (
+    <Listing items={FilteredLisingItems}/>
+  )
+}
+
+
+export default App
+export type {ListingItemData}
--- src/Listing.tsx
+++ src/Listing.tsx
@@ -1,8 +1,27 @@
-import type {ListingItem} from './App'
+import type {ListingItemData} from './App'
 
 
-function Listing(items:ListingItem[]) {
-
+function Listing({items}:ListingItemData[]=[]) {
+    console.log(items)
+    return (
+        <div className='item-list'>
+            {items.map(listingItemData => (
+                <div className='item'>
+                    <div className='item-image'>
+                        <a href={listingItemData.url}>
+                            <img src={listingItemData.MainImage_url_570xN} />
+                        </a>
+                    </div>
+                    <div className='item-details'>
+                        <p className="item-title">{listingItemData.title}</p>
+                        <p className="item-price">{listingItemData.currency_code}{listingItemData.price}</p>
+                        <p className={'item-quantity level-' + (listingItemData.quantity <= 10 ? 'low' : 
+                                                                listingItemData.quantity <= 20 ? 'medium' : 'high' )}>{listingItemData.quantity} left</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 
